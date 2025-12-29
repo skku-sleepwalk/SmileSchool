bff7693366cebc851b8972bdde2cf4ddf31d19b4
류현승 <I39410823@gmail.com>
2025-12-29 19:57:54 +0900

    1229 move

---
+++ Assets/Left.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Left : MonoBehaviour
+{
+    float x = 0;
+    float y = 0;
+    float speed = 2;
+    bool flip = false;
+    Rigibody2D rd2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
