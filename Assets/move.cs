28e6139fdc4ba8f645e3b61ea5dec030cb3877ae
류현승 <I39410823@gmail.com>
2026-01-05 19:09:07 +0900

    move

---
+++ Assets/move.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    float x = 0;
+    float y = 0;
+    float speed = 2;
+    bool flip = false;
+    Rigidbody2D rb2D;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = 0;
+        y = 0;
+        if (Input.GetKey("right"))
+        {
+            rb2D.velocity = new Vector2(speed, 0);
+        }
+        else if (Input.GetKey("left"))
+        {
+            rb2D.velocity = new Vector2(-speed, 0); ;
+        }
+    }
+       
+       
+
+}
