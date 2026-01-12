68a6895fe7c24ad3fdb6c55b6a4ff1bb30a84a12
고은 <ggoeun018@gmail.com>
2026-01-12 18:13:48 +0900

    ui

---
+++ Assets/Avoidance.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avoidance : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+   public void AvoidanceEnemy()
+    {
+        Debug.Log("적을 피했다!");
+    }
+
+}
---
+++ Assets/kill.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kill : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+
+   public void killEnemy()
+    {
+        Debug.Log("적을 죽였다!");
+    }
+
+}
