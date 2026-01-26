cd3d42556acddf47bf477b8ec1c2393ec0a7676e
류현승 <I39410823@gmail.com>
2026-01-26 19:15:15 +0900

    kill

--- Assets/kill.cs
+++ Assets/kill.cs
@@ -6,10 +6,22 @@ public class kill : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    public Transform Character;
+    Vector2 direction = Vector2.right;
+    float distance = 2f;
 
-   public void killEnemy()
+    public void killEnemy() 
     {
+        Vector2 origin = Character.position;
+        RaycastHit2D hit = Physics2D.Raycast(origin, direction, distance);
+        Debug.Log(hit.collider.gameObject);
+        if (hit.collider.gameObject.tag == "monster")
+        {
+            Destroy(hit.collider.gameObject);
+        }
         Debug.Log("ÀûÀ» Á×¿´´Ù!");
     }
+    
+    //Destroy(Monster);
 
 }
