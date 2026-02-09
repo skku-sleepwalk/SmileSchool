fe13f14632d50b5061dda7b8dbe9cb37d42b7e1b
류현승 <I39410823@gmail.com>
2026-02-09 21:00:48 +0900

    ssss

--- Assets/kill.cs
+++ Assets/kill.cs
@@ -1,22 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+
 
 public class kill : MonoBehaviour
 {
     // Start is called before the first frame update
-
+    private bool isCool = false;
     public Transform Character;
     Vector2 direction = Vector2.right;
     float distance = 2f;
     int killCount = 0;
     public List<string> SceneList = new List<string>();
     int index = 0;
+    public SpriteRenderer sq;
+    public Button bt;
+    public void Start()
+    {
+        bt = GetComponent<Button>();
+    }
     public void killEnemy() 
     {
+        if (isCool == true) return;
+        isCool = true;
         Vector2 origin = Character.position;
         RaycastHit2D hit = Physics2D.Raycast(origin, direction, distance);
+        ColorBlock colorBlock = bt.colors;
+        colorBlock.selectedColor = new Color(150f / 255f, 150f / 255f, 150f / 255f, 255f / 255f);
+        bt.colors = colorBlock;
+        sq.color = new Color(1f, 1f, 1f, 1f);
+        StartCoroutine("Invincibility");
 
         if (hit.collider.gameObject.tag == "monster")
         {
@@ -36,5 +49,22 @@ public class kill : MonoBehaviour
     }
     
     //Destroy(Monster);
+        }
+        Debug.Log("ÀûÀ» Á×¿´´Ù!");
+    }
+    IEnumerator Invincibility()
+    {
+        yield return new WaitForSeconds(1f);
+        sq.color = new Color(1f, 1f, 1f, 0f);
+        ColorBlock colorBlock = bt.colors;
+        isCool = false;
+        colorBlock.selectedColor = new Color(1f, 1f, 1f, 1f);
+        bt.colors = colorBlock;
 
+
+        //Destroy(Monster);
+
+    }
+   
 }
+
